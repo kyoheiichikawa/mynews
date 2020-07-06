887c7437557cede14ae195a8e14e3e748de11ecd
KYOHEI <kyoheiichikawa324@gmail.com>
2020-07-06 06:28:49 +0000

    for_homework09&10

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,11 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
 });
 
-
 Route::get('xxx', 'AAAController@bbb');
 
-Route::get('admin/profile/create', 'Admin\ProfileController@add');
+Route::group(['prefix' => 'admin'], function() {
+     Route::get('profile/create', 'Admin\ProfileController@add');
+     Route::get('profile/edit', 'Admin\ProfileController@edit');
+});
 
-Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
+
